cb0bc75b1cedd1c46cc9f6401aa7ec6be387dec2
bezvozni <prostoprostor@gmail.com>
2022-02-14 22:04:33 +0200

    Обернули содержимое subs_pars.py в функцию
    + подключили эту функцию к main.py
    Зачем: теперь скрипт скачивает не все видео сразу
    и только потом ищет в них. А скачивает первое, ищет в нём,
    скачивает второе, ищет в нём .. и так далее.
    Это подготовка к работе с ботом
    (когда пользователь захочет максимально быстро первый результат)
    
    + подаю на вход ТОЛЬКО автоматические субтиры
    (другие скрипт не понимает)
    + для точности файлы с субтитрами называются по video_id
    + включён тихий режим, принтим по минимуму
    
    + исправлен не очевидный баг:
    скрипт выдавал таймкод строки(параграфа), а не слова
    + окончательно прибит баг:
    теперь скрипт точно выдаёт таймод искомого слова
    (а не предыдущего)

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from yt_dlp import YoutubeDL
+from subs_parse import search_query
+import os
 
-url = 'https://www.youtube.com/channel/UCrV_cFYbUwpjSOPVJOjTufg/videos'
+path_subs = os.path.abspath('./subs')
+
+url = 'https://www.youtube.com/c/DesigningTheFuture'
+query = 'общей'
+
+with YoutubeDL({"extract_flat": True, "skip_download": True}) as ydl:
+    res = ydl.extract_info(url)
 
 ydl_opts = {
-    "writesubtitles": True,
+    #  "writesubtitles": True,
     "writeautomaticsub": True,
     "subtitleslangs": ['ru'],
     "subtitlesformat": 'srv3',
-    "outtmpl": './subs/%(title)s.f%(format_id)s.%(ext)s',
-    "skip_download": True
+    "outtmpl": './subs/%(id)s',
+    "skip_download": True,
+    "quiet": True
 }
 
-with YoutubeDL(ydl_opts) as ydl:
-    ydl.download([url])
+for i in res["entries"]:
+    if not os.path.isfile('./subs/' + i["id"] + '.ru.srv3'):
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download(i["url"])
+    if os.path.isfile('./subs/' + i["id"] + '.ru.srv3'):
+        search_query(query, path_subs + os.sep + i["id"] + ".ru.srv3")
--- subs_parse.py
+++ subs_parse.py
@@ -1,54 +1,58 @@
 import os
+from os import sep
 from bs4 import BeautifulSoup
 
-# Тут внимательно! Следить руками:
-# что бы video_id в yt_link совпадало с video_id первого файла в директории path
-yt_link = "https://www.youtube.com/watch?v=0TJLkZZd_ds&t="
-search_words = "вами"
 
-# Get the list of all files and directories
-path = "./subs/"
-dir_list = os.listdir(path)
+def search_query(search_words, file):
+    video_id = file.split(sep)[-1].split('.')[0]
+    yt_link = f"https://www.youtube.com/watch?v={video_id}&t="
 
-all_words = []
-all_times = []
+    all_words = []
+    all_times = []
 
-# код ниже ищет заданные search_words
-# ТОЛЬКО в автоматически сгенерированных субтитрах
-# и ТОЛЬКО в первом файле директории subs
-for file in dir_list[:1]:
-    with open(path+file, encoding="utf-8") as f:
+    with open(file, encoding="utf-8") as f:
         soup = BeautifulSoup(f, 'html.parser')
 
         paragraphes = soup.find_all('p')
         for paragraph in paragraphes:
-            p_time = paragraph.get('t')
+            p_time = int(paragraph.get('t'))
 
             words = paragraph.find_all('s')
             if not words:
                 continue
 
             for word in words:
-                w_time = paragraph.get('t')
-                w_time = w_time if not None else p_time
+                w_time = word.get('t')
+                if w_time is None:
+                    w_time = 0
+                else:
+                    w_time = int(w_time)
 
                 all_words.append(word.text.strip())
-                all_times.append(w_time)
+                all_times.append(w_time + p_time)
 
-timeByWordIndex = {}
-string_of_all_words = ''
-for idx, word in enumerate(all_words):
-    string_of_all_words += word + ' '
-    timeByWordIndex[len(string_of_all_words)] = all_times[idx]
+    timeByWordIndex = {}
+    string_of_all_words = ''
+    for idx, word in enumerate(all_words):
+        timeByWordIndex[len(string_of_all_words)] = all_times[idx]
+        string_of_all_words += word + ' '
 
-res = -1
-answers = []
-while True:
-    res = string_of_all_words.find(search_words, res + 1)
-    if res == -1:
-        break
-    index = string_of_all_words[:res].rfind(' ') + 1
-    answers.append(timeByWordIndex[index])
+    res = -1
+    answers = []
+    while True:
+        res = string_of_all_words.find(search_words, res + 1)
+        if res == -1:
+            break
+        index = string_of_all_words[:res].rfind(' ') + 1
+        answers.append(timeByWordIndex[index])
 
-for timems in answers:
-    print(yt_link + timems + 'ms')
+    for timems in answers:
+        print(yt_link + str(timems) + 'ms')
+
+
+def main(query, file):
+    search_query(query, file)
+
+
+if __name__ == '__main__':
+    main()
